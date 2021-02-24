e387d726043ce4114d384f35b3e5253c038e4e87
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-23 17:04:41 -0800

    init

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const pool = require('./lib/utils/pool');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   // eslint-disable-next-line no-console
+//   console.log(`Started on ${PORT}`);
+// });
 
-process.on('exit', () => {
-  console.log('Goodbye!');
-  pool.end();
-});
+// process.on('exit', () => {
+//   console.log('Goodbye!');
+//   pool.end();
+// });
